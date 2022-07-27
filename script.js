0eb3c400ed15fbcaf0ad20c72d50f3ebdd38a38f
pretzelogic <yingyangyo266@gmail.com>
2022-07-26 19:19:22 -0500

    add removeElements

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container = document.querySelector('#container')
+
 function addElements(amount){
     for (let i = 0; i < amount; i++){
         const aArea = document.createElement('div')
         aArea.setAttribute('id', 'pixel')
         container.appendChild(aArea)
     }
+}
+
+function removeElements(){
+    const canvas = document.querySelectorAll('#pixel')
+    canvas.forEach(pixel => {
+        pixel.remove()
+    });
 }
 
 // function changeColor(e){
@@ -14,18 +22,20 @@ function addElements(amount){
 
 
 const button = document.querySelector('#new')
-console.log(button)
 button.addEventListener('click', (e) => {
     console.log(e)
     let amount = prompt('enter amount')
     addElements(amount)
+
     const canvas = document.querySelectorAll('#pixel')
     console.log(canvas)
+
     canvas.forEach((pixel) => {
         pixel.addEventListener('mouseover', (e) =>{
             pixel.style.backgroundColor = 'red'
         })
     })
+
     canvas.forEach((pixel) => {
         pixel.addEventListener('mouseleave', (e) =>{
             pixel.style.backgroundColor = 'lightblue'
