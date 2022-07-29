f4896258f6fc0bfe0754a978c71469e24ba815c2
pretzelogic <yingyangyo266@gmail.com>
2022-07-29 14:16:12 -0500

    complete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('#container')
 
-function addElements(amount){
+function addElements(x){
+    let amount = x*x
     for (let i = 0; i < amount; i++){
         const aArea = document.createElement('div')
         aArea.setAttribute('id', 'pixel')
@@ -15,30 +16,19 @@ function removeElements(){
     });
 }
 
-// function changeColor(e){
-//     e.style.backgroundColor = 'red'
-//     console.log(e)
-// }
 
 
 const button = document.querySelector('#new')
 button.addEventListener('click', (e) => {
-    console.log(e)
     let amount = prompt('enter amount')
+    removeElements()
     addElements(amount)
 
     const canvas = document.querySelectorAll('#pixel')
-    console.log(canvas)
 
     canvas.forEach((pixel) => {
         pixel.addEventListener('mouseover', (e) =>{
-            pixel.style.backgroundColor = 'red'
-        })
-    })
-
-    canvas.forEach((pixel) => {
-        pixel.addEventListener('mouseleave', (e) =>{
-            pixel.style.backgroundColor = 'lightblue'
+            pixel.style.backgroundColor = 'black'
         })
     })
 })
